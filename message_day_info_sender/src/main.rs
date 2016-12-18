3bd6e65377c6bd48522d26d2b577e53f48504c65
Björn Nyberg <bjorn.nyberg@gmail.com>
2016-12-18 21:30:15 +0100

    RocksDB for day info sender

--- message_day_info_sender/src/main.rs
+++ message_day_info_sender/src/main.rs
@@ -8,15 +8,17 @@ extern crate serde_json;
 extern crate hyper;
 extern crate nanomsg;
 extern crate time;
+extern crate rocksdb;
 
 use nanomsg::{Socket, Protocol};
 
 use std::thread;
 use std::time::{Duration};
 use std::io::{Read, Write};
-use hyper::client::{Client, Request, Response};
-use hyper::header::Connection;
-use std::env;
+use hyper::client::{Client};
+use rocksdb::{DB, Direction, IteratorMode};
+use std::str;
+
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Dagar {
@@ -28,7 +30,7 @@ struct Dagar {
     dagar: Vec<DagarInfo>
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default)]
 struct DagarInfo {
     datum: String,
     veckodag: String,
@@ -38,23 +40,57 @@ struct DagarInfo {
 }
 
 
-struct Forecast {
-
+fn fetch_today_and_store_month(db: &DB, date: String) -> DagarInfo {
+    let client = Client::new();
+    let start = time::now();
+    let uri_of_the_day = format!("http://api.dryg.net/dagar/v2.1/{}/{}", start.tm_year + 1900, start.tm_mon+1);
+    println!("{}", uri_of_the_day);
+    let mut res = client.get(uri_of_the_day.as_str()).send().unwrap();
+    let mut buffer = String::new();
+    res.read_to_string(&mut buffer).unwrap();
+    println!("{:?}", buffer);
+    let deserialized_data: Dagar = serde_json::from_str(&mut buffer).unwrap();
+    let mut today: DagarInfo = Default::default();
+    for day in deserialized_data.dagar {
+        db.put(day.datum.as_bytes(), serde_json::to_string(&day).unwrap().as_bytes());
+        if day.datum == date {
+            today = day;
+        }
+    }
+    let iter = db.iterator(IteratorMode::From(date.as_bytes(), Direction::Reverse)); // From a key in Direction::{forward,reverse}
+    for (key, _) in iter {
+        db.delete(key.as_ref());
+    }
+    return today;
 }
+
 fn main() {
+    let db = DB::open_default("rocksdb_storage").unwrap();
+
     //let url ="ipc:///tmp/pubsub.ipc";
     let url = "tcp://127.0.0.1:8021";
     let mut socket = Socket::new(Protocol::Pub).unwrap();
     let mut endpoint = socket.connect(url).unwrap();
-    let mut count = 1u32;
 
     match socket.set_ipv4_only(true) {
         Ok(..) => {},
         Err(err) => panic!("Failed to change ipv4 only on the socket: {}", err)
     }
-    let client = Client::new();
+
     let start = time::now();
-    println!("{} {} {}", start.tm_year + 1900, start.tm_mon+1, start.tm_mday);
+    let date =format!("{}-{}-{}", start.tm_year + 1900, start.tm_mon+1, start.tm_mday);
+
+    let mut today: DagarInfo = Default::default();
+    match db.get(date.as_bytes()) {
+        Ok(Some(value)) => today = serde_json::from_str(value.to_utf8().unwrap()).unwrap(),
+        Ok(None) => {
+                        today = fetch_today_and_store_month(&db, date);
+        },
+        Err(e) => println!("operational problem encountered: {}", e),
+    }
+
+
+    /*
     let uri_of_the_day = format!("http://api.dryg.net/dagar/v2.1/{}/{}/{}", start.tm_year + 1900, start.tm_mon+1, start.tm_mday);
     println!("{}", uri_of_the_day);
     let mut res = client.get(uri_of_the_day.as_str()).send().unwrap();
@@ -64,11 +100,13 @@ fn main() {
     let deserialized_data: Dagar = serde_json::from_str(&mut buffer).unwrap();
     println!("{:?}", deserialized_data);
     println!("Server is ready.");
-    let namnsdagsbarn: String = deserialized_data.dagar[0].namnsdag.join(", ");
-    let msg = format!("{} {}| Vecka {}|{}",
-                      deserialized_data.dagar[0].datum, deserialized_data.dagar[0].veckodag,
-                      deserialized_data.dagar[0].vecka,
+    */
+    let namnsdagsbarn: String = today.namnsdag.join(", ");
+    let msg = format!("{} {}|Vecka {}|{}",
+                      today.datum, today.veckodag,
+                      today.vecka,
                       namnsdagsbarn);
+    
 
     loop {
         match socket.write_all(msg.as_bytes()) {
@@ -79,7 +117,6 @@ fn main() {
             }
         }
         thread::sleep(Duration::from_millis(4000));
-        count += 1;
     }
 
     endpoint.shutdown();
